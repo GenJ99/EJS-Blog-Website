6f4b5e019b4fdbf5fa583fa247dabd680f4ff9cd
GenJay99 <jasongencarella1@gmail.com>
2019-09-16 15:06:29 -0400

    Print title element from posts array to conole

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
-    res.render("home", {startingContent: homeStartingContent, newContent: posts});
+    res.render("home", {startingContent: homeStartingContent, posts: posts});
 
 });
 
